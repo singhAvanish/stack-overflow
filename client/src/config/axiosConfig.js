87741812eb9b65b8811304d7774ee5e5ac1781e2
singhAvanish <88013566+singhAvanish@users.noreply.github.com>
2024-06-08 16:51:55 +0530

    Update axiosConfig.js

--- client/src/config/axiosConfig.js
+++ client/src/config/axiosConfig.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API = axios.create({ baseURL: 'http://localhost:4000' });
+const API = axios.create({ baseURL: 'https://stack-overflow-81w9.onrender.com' });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem("Profile")) {
